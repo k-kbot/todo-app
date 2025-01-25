973ecbb9ee50f2f56110f323352c29552f43156a
k-kbot <polykkpodh1243@gmail.com>
2025-01-25 18:59:20 +0900

    GET /todos

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
 import { Hono } from "hono";
+import { drizzle } from "drizzle-orm/d1";
+import { todos } from "../db/schema";
 
-const app = new Hono();
+type Bindings = {
+	DB: D1Database;
+};
+
+const app = new Hono<{ Bindings: Bindings }>().basePath("/api");
 
 app.get("/", (c) => {
 	return c.text("Hello Hono!");
+});
+
+app.get("/todos", async (c) => {
+	const db = drizzle(c.env.DB);
+	const allTodos = await db.select().from(todos).all();
+	return c.json(allTodos);
 });
 
 export default app;
