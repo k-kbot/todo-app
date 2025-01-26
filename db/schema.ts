7416c1016178863362944a829a60a59d8c090d66
k-kbot <polykkpodh1243@gmail.com>
2025-01-26 14:57:49 +0900

    add users table

--- db/schema.ts
+++ db/schema.ts
@@ -1,7 +1,27 @@
 import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
+import { relations } from "drizzle-orm";
+
+export const users = sqliteTable("users", {
+	id: integer("id").primaryKey(),
+	name: text("name").notNull(),
+	email: text("email").notNull().unique(),
+	password: text("password").notNull(),
+});
+
+export const usersRelations = relations(users, ({ many }) => ({
+	posts: many(todos),
+}));
 
 export const todos = sqliteTable("todos", {
 	id: integer("id").primaryKey(),
+	userId: integer("userId").notNull(),
 	title: text("title").notNull(),
 	completed: integer("completed", { mode: "boolean" }).notNull(),
 });
+
+export const todosRelations = relations(todos, ({ one }) => ({
+	author: one(users, {
+		fields: [todos.userId],
+		references: [users.id],
+	}),
+}));
---
+++ drizzle/0001_thankful_nighthawk.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `users` (
+	`id` integer PRIMARY KEY NOT NULL,
+	`name` text NOT NULL,
+	`email` text NOT NULL,
+	`password` text NOT NULL
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `users_email_unique` ON `users` (`email`);--> statement-breakpoint
+ALTER TABLE `todos` ADD `userId` integer NOT NULL;
