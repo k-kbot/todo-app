857193a14222e0087f49f84aed976da04ea54cda
k-kbot <polykkpodh1243@gmail.com>
2025-01-26 16:46:20 +0900

    POST /login

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,69 @@ import { drizzle } from "drizzle-orm/d1";
 import { todos, users } from "../db/schema";
 import { z } from "zod";
 import bcrypt from "bcryptjs";
+import { sign } from "hono/jwt";
+import { eq } from "drizzle-orm";
+
+import { env } from "hono/adapter";
 
 type Bindings = {
 	DB: D1Database;
+	JWT_SECRET: string;
 };
 
 const app = new Hono<{ Bindings: Bindings }>().basePath("/api");
 
 app.get("/", (c) => {
 	return c.text("Hello Hono!");
+});
+
+const postLoginSchema = z.object({
+	email: z.string().email(),
+	password: z.string().min(8),
+});
+
+app.post("/login", async (c) => {
+	const db = drizzle(c.env.DB);
+	const body = await c.req.json();
+
+	const validationResult = postLoginSchema.safeParse(body);
+	if (!validationResult.success) {
+		return c.json(
+			{
+				error: "Validation failed",
+				details: validationResult.error.format(),
+			},
+			400,
+		);
+	}
+
+	const { email, password } = validationResult.data;
+	const [user] = await db.select().from(users).where(eq(users.email, email));
+
+	if (user === undefined) {
+		return c.json({ error: "Invalid email or password" }, 401);
+	}
+
+	const isValidPassword = await bcrypt.compare(password, user.password);
+	if (!isValidPassword) {
+		return c.json({ error: "Invalid email or password" }, 401);
+	}
+
+	const payload = {
+		sub: user.id,
+		exp: Math.floor(Date.now() / 1000) + 60 * 60,
+	};
+	const { JWT_SECRET } = env<{ JWT_SECRET: string }>(c);
+	const token = await sign(payload, JWT_SECRET);
+
+	return c.json(
+		{
+			access_token: token,
+			token_type: "Bearer",
+			expires_in: payload.exp,
+		},
+		200,
+	);
 });
 
 const postUsersSchema = z.object({
